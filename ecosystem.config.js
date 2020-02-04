ec1d4abd8568c762c60a74890ddc64483630e995
Thomas Schäfer <t.schaefer@headtrip.eu>
2020-02-04 10:15:00 +0100

    change ecosystem name to avoid conflicts with old version

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   apps : [{
-    name: 'whitespire',
+    name: 'whitespire-next',
     script: `PORT=${process.env.PORT} npm run start:next`,
     instances: 1,
     autorestart: true,
